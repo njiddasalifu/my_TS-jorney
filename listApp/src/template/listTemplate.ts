35f1980cc0cfc3f1a84db1184f3b8e402846e0da
njiddasalifu <njiddasalifu@gmail.com>
2023-04-26 13:05:52 +0100

    template

---
+++ listApp/src/template/listTemplate.ts
@@ -0,0 +1,26 @@
+import FullList from "../models/FullList";
+interface DOMList{
+    ul: HTMLDataListElement,
+    clear(): void,
+    render(fullList: FullList): void,
+}
+
+export default class ListTemplate implements DOMList{
+    ul: HTMLUListElement
+    private constructor(){
+        this.ul = document.getElementById("listItems") as HTMLUListElement
+    }
+
+    clear(): void {
+        this.ul.innerHTML = ''  
+    }
+
+    //reder method which receieves the full list
+    render(fullList: FullList): void {
+        this.clear()
+        fullList.list.forEach(=>{
+            const li = document.createElement("li") as HTMLImageElement
+            li.className = "item"
+        })
+    }
+}
