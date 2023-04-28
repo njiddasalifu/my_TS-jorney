abb666995c7f8ea9e8d20888fddc23938b35ad52
njiddasalifu <njiddasalifu@gmail.com>
2023-04-28 22:57:42 +0100

    calling all files and functions in the main.ts

--- listApp/src/main.ts
+++ listApp/src/main.ts
@@ -1,1 +1,10 @@
 import './css/style.css'
+import FullList from './models/FullList'
+import ListItem from './models/ListItem'
+import ListTemplate from './template/listTemplate'
+
+
+const initApp = ()=>{
+
+}
+document.addEventListener("DOMContentLoaded", initApp)
