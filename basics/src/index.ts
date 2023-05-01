93eaab6497c8dffb99e96ce07e1f1206ec37e89e
njiddasalifu <njiddasalifu@gmail.com>
2023-05-01 01:09:38 +0100

    funstions

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,3 +1,9 @@
+//basics types
+
 let id: number = 5
- 
-//id = '5'  
+let company: string = 'Salif Web'
+let isPublished: boolean= true
+
+let x:any = 'Hello' // any type can work
+
+let ids: number[] =  []
