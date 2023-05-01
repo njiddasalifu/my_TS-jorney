de4463c3abb94bb2360c89ea84438040afd6ac01
njiddasalifu <njiddasalifu@gmail.com>
2023-05-01 01:09:13 +0100

    functions addNum

---
+++ basics/dist/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+//functions
+function addNum(x, y) {
+    return x + y;
+}
+//caling the function
+console.log(addNum(2, 2));
--- basics/dist/index.js
+++ basics/dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
+//basics types
 let id = 5;
-//id = '5'  
+let company = 'Salif Web';
+let isPublished = true;
+let x = 'Hello';
